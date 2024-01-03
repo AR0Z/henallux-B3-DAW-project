23acfa8ddb42c836b398853a2509d48604e5b2aa
TH3RY <therygeek@gmail.com>
2024-01-03 19:40:58 +0100

    tests

--- src/main/java/com/spring/henallux/ecommerce/dataAccess/entity/OrderEntity.java
+++ src/main/java/com/spring/henallux/ecommerce/dataAccess/entity/OrderEntity.java
@@ -4,6 +4,7 @@ import com.spring.henallux.ecommerce.dataAccess.entity.UserEntity;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
@@ -27,4 +28,8 @@ public class OrderEntity {
     @JoinColumn(name = "user_id")
     private UserEntity userId;
 
+    public String toString() {
+    	return "OrderEntity [id=" + id + ", date=" + date + ", paypalOrderId=" + paypalOrderId + ", orderStatus="
+    			+ orderStatus + ", userId=" + userId.getId() + "]";
+    }
 }
