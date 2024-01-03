c56df76297c2141a949c2a13fe17183979f06122
Aroz - Germain <AR0Z@protonmail.com>
2024-01-03 18:06:53 +0100

    Update OrderLineRepository interface

--- src/main/java/com/spring/henallux/ecommerce/dataAccess/repository/OrderLineRepository.java
+++ src/main/java/com/spring/henallux/ecommerce/dataAccess/repository/OrderLineRepository.java
@@ -1,7 +1,7 @@
 package com.spring.henallux.ecommerce.dataAccess.repository;
 
-import com.spring.henallux.ecommerce.dataAccess.entity.OrderEntity;
+import com.spring.henallux.ecommerce.dataAccess.entity.OrderLineEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderLineRepository extends JpaRepository<OrderEntity, String> {
+public interface OrderLineRepository extends JpaRepository<OrderLineEntity, String> {
 }
